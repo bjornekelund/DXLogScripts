0814cab515b1dfe895c04d526e3e2980b4228b4e
Björn Ekelund <bjorn@ekelund.nu>
2020-06-15 12:48:41 +0200

    Improve EqualizeVFO

--- DXLogScripts/EqualizeVFO.cs
+++ DXLogScripts/EqualizeVFO.cs
@@ -1,8 +1,10 @@
 //INCLUDE_ASSEMBLY System.dll
 //INCLUDE_ASSEMBLY System.Windows.Forms.dll
+//INCLUDE_ASSEMBLY IOComm.dll
 
-// Script to force radio 1's VFO B to frequency of VFO A
-// By Björn Ekelund SM7IUN sm7iun@ssa.se 2020-06-14
+// Script to force other VFO of radio to same frequency as currently selected VFO 
+// Works for all supported operating techniques
+// By Björn Ekelund SM7IUN sm7iun@ssa.se 2019-01-30
 
 using IOComm;
 
@@ -16,9 +18,15 @@ namespace DXLog.net
 
         public void Main(FrmMain main, ContestData cdata, COMMain comMain)
         {
-            double _newFreq = cdata.Radios[1].Freq[0];
-            main.SetCATFrequency(1, "B", _newFreq);
+            double frequency;
+            int focusedradio, vfo;
+            string othervfo;
 
+            focusedradio = cdata.FocusedRadio; // 1 or 2
+            vfo = cdata.Radios[focusedradio].ActiveVFO; // 0 = VFO A, 1 = VFO B
+            frequency = cdata.Radios[focusedradio].Freq[vfo];
+            othervfo = vfo == 0 ? "B" : "A";
+            main.SetCATFrequency(focusedradio, othervfo, frequency);
         }
     }
 }
