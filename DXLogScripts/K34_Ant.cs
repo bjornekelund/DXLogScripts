c54379085188c27127f37969bc40d11b952b3e35
Björn Ekelund <bjorn@ekelund.nu>
2022-10-12 16:39:15 +0200

    Update

--- DXLogScripts/K34_Ant.cs
+++ DXLogScripts/K34_Ant.cs
@@ -1,6 +1,5 @@
 //INCLUDE_ASSEMBLY System.dll
 //INCLUDE_ASSEMBLY System.Windows.Forms.dll
-//INCLUDE_ASSEMBLY IOComm.dll
 
 // Elecraft K3/K4 RX antenna toggling.
 // By Bob Wilson N6TV n6tv@arrl.net 2 October 2022 20:53 UTC
@@ -22,22 +21,25 @@ namespace DXLog.net
 
         // Executes at macro invocation
         public void Main(FrmMain main, ContestData cdata, COMMain comMain)
-        {			
+        {
             int radio;
+            CATCommon radioobject;
 
             if (cdata.OPTechnique == ContestData.Technique.SO1R || cdata.OPTechnique == ContestData.Technique.SO2V)
             {
                 radio = 1;
+                radioobject = main.COMMainProvider._radio1Object;
             }
             else
             {
                 radio = cdata.FocusedRadio;
+                radioobject = radio == 1 ? main.COMMainProvider._radio1Object : main.COMMainProvider._radio2Object;
             }
 
-            CATCommon radioobject = (radio == 1) ? main.COMMainProvider._radio1Object : main.COMMainProvider._radio2Object;
-
             string HOSTcmd = string.Empty;
-            string radiotype = CATCommon.RadioID;
+            string radiotype = radioobject.RadioType();
+
+            //main.SetMainStatusText(string.Format("K34RxAnt: Radio type {0}.", radiotype));
 
             if (radioobject != null)
             {
